8eb6fcaee41c3443786d1919b799457d239f7598
spartypkp <will@diamondquarters.com>
2024-06-17 12:38:33 -0700

    push changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async redirects() {
-        return [
-            process.env.MAINTENANCE_MODE === "1"
-                ? {
-                    source: "/((?!maintenance.html).*)",
-                    destination: "/maintenance.html",
-                    permanent: false,
-                  }
-                : null,
-        ].filter(Boolean);
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
